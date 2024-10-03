89c7cd2396aa91d41401f8cf1ff7bda95bb35639
Maria <mariaeduardamoraisdias615@gmail.com>
2024-10-03 16:16:52 -0300

    Atividade

---
+++ Adivinhação de numeros/Program.cs
@@ -0,0 +1,33 @@
+﻿
+		Random random = new Random(); // gerador automatico
+		int numeroSecreto = random.Next(1, 101); // variavel de numeros inteiros
+		int palpite = 0; // variavel de numeros inteiros
+		int tentativas = 0; // variavel de numeros inteiros
+		Console.WriteLine("Tente adivinhar o número entre 1 e 100!"); //impressao de tela
+		while (palpite != numeroSecreto) // executa uma instrucao
+		{
+			Console.Write("Digite seu palpite: ");//impressao de tela
+			string entrada = Console.ReadLine(); //e uma cadeia  de caracteres que representa  um tipo de valor 
+			if (int.TryParse(entrada, out palpite)) // estrutura de decisoes
+			{
+				tentativas++;
+				if (palpite < numeroSecreto)// e uma estrutura  de decisoes
+				{
+					Console.WriteLine("Muito baixo! Tente novamente.");//impressao de tela
+				}
+
+				if (palpite > numeroSecreto)// e uma estrutura de decisoes
+				{
+					Console.WriteLine("Muito alto! Tente novamente.");//impressao de tela
+				}
+				else if (palpite == numeroSecreto)// estrutura de decisoes
+				{
+					Console.WriteLine($"Parabéns! Você adivinhou em {tentativas} tentativas.");//impressao de tela
+				}
+			}
+			else // estrutura de decisoes
+			{
+				Console.WriteLine("Por favor, insira um número válido!");//impressao de tela
+			}
+		}
+	
